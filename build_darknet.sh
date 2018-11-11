42f2e5c6afbfc5e4e39179ab6bc86a7ec0f2523a
dweeptrivedi <dweeptrivedi1994@gmail.com>
2018-11-11 04:54:38 +0000

    modify build script to print output

--- build_darknet.sh
+++ build_darknet.sh
@@ -1,8 +1,8 @@
 cd train/darknet/
-make -j20
+make -j20  &> buid.log && echo "cpu build success!!!" || echo "cpu build failure!!!"
 cp libdarknet.so ../../src/libdarknet_cpu.so
 cp Makefile.gpu Makefile
-make -j20
+make -j20 &> build.log && echo "gpu build success!!!" || echo "gpu build failure!!!"
 cp libdarknet.so ../../src/libdarknet_gpu.so
 cp Makefile.cpu Makefile
 cd -
